4d871af5e41ac45ea99b11920936a09a278ff856
MiguelAVL09 <mikelun3@gmail.com>
2025-11-27 19:34:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ chatForm.addEventListener('submit', async (e) => {
 
     try {
         // 3. Petición al backend
-        const response = await fetch('http://localhost:3000/chat', {
+        const response = await fetch('https://TU-NOMBRE-DE-PROYECTO.onrender.com/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: text }),
